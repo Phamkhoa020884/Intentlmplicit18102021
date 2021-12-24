bca8a7529376dd98d29985719eeb897ddc55f461
khoa <hatgiong@gmail.com>
2021-12-24 18:53:11 +0700

    Initial commit

---
+++ app/src/main/java/com/example/intentlmplicit18102021/MainActivity.java
@@ -0,0 +1,101 @@
+package com.example.intentlmplicit18102021;
+
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.provider.Settings;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity{
+
+    Button mBtnGallery, mBtnCamera;
+    ImageView mImg;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mBtnGallery = findViewById(R.id.buttonGallery);
+        mBtnCamera = findViewById(R.id.buttonCamera);
+        mImg = findViewById(R.id.imageView);
+
+
+        mBtnCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                requestLauncherCamera.launch(Manifest.permission.CAMERA);
+            }
+        });
+    }
+
+    private ActivityResultLauncher<String> requestLauncherCamera = registerForActivityResult(new ActivityResultContracts.RequestPermission(), new ActivityResultCallback<Boolean>() {
+        @Override
+        public void onActivityResult(Boolean result) {
+            if (result) {
+                requestLauncherOpenCamera.launch(new Intent(MediaStore.ACTION_IMAGE_CAPTURE));
+            } else {
+                if (shouldShowRequestPermissionRationale(Manifest.permission.CAMERA)) {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                    builder.setTitle("Camera permission go to setting enable");
+                    builder.setPositiveButton("Setting", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                            Uri uri = Uri.fromParts(
+                                    "package",
+                                    BuildConfig.APPLICATION_ID, null
+                            );
+                            intent.setData(uri);
+                            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                            startActivity(intent);
+                        }
+                    });
+                    builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            requestLauncherCamera.launch(Manifest.permission.CAMERA);
+                        }
+                    }).show();
+                } else {
+                    Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                    Uri uri = Uri.fromParts(
+                            "package",
+                            BuildConfig.APPLICATION_ID, null
+                    );
+                    intent.setData(uri);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    startActivity(intent);
+                }
+            }
+        }
+    });
+
+    private ActivityResultLauncher<Intent> requestLauncherOpenCamera = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
+        @Override
+        public void onActivityResult(ActivityResult result) {
+            if (result.getResultCode() == RESULT_OK && result.getData() != null){
+                Bitmap bitmap = (Bitmap) result.getData().getExtras().get("data");
+                mImg.setImageBitmap(bitmap);
+            }
+        }
+    });
+}
